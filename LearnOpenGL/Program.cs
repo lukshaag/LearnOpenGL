ee0a2f30e72d4107bc3f44e76621fd0d482bd7c4
LuKsHaaG <LuKsHaaG@DESKTOP-HSO9PRR>
2025-04-07 13:52:14 -0300

    Adicionar arquivos de projeto.

---
+++ LearnOpenGL/Program.cs
@@ -0,0 +1,47 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace LearnOpenGL;
+
+public class Program : GameWindow {
+    //settings
+    const int SCR_WIDTH = 800;
+    const int SCR_HEIGHT = 600;
+    //creating window glfw
+    private Program(GameWindowSettings gws, NativeWindowSettings nws) : base(gws, nws) { 
+
+    }
+
+    protected override void OnFramebufferResize(FramebufferResizeEventArgs e) {
+        framebuffer_size_callback(SCR_WIDTH, SCR_HEIGHT);
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs args) {
+        //input
+        processInput();
+
+        SwapBuffers();
+    }
+
+    private static void Main(string[] args) {
+        GameWindowSettings gws = GameWindowSettings.Default;
+
+        NativeWindowSettings nws = NativeWindowSettings.Default;
+        nws.ClientSize = ( SCR_WIDTH, SCR_HEIGHT );
+        nws.Title = "LearnOpenGL";
+
+        new Program(gws, nws).Run();
+    }
+
+    private void processInput() { 
+        if(KeyboardState.IsAnyKeyDown(Keys.Escape)) {
+            Close();
+        }
+    }
+
+    private static void framebuffer_size_callback(int width, int height) {
+        GL.Viewport(0, 0, width, height);
+    }
+}
